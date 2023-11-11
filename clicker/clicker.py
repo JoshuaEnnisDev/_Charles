5bb55589c3e8e865231286e30481f9e7a66b981b
JoshuaEnnisDev <joshuaennisdev@gmail.com>
2023-11-10 19:37:38 -0600

    commit

--- clicker/clicker.py
+++ clicker/clicker.py
@@ -3,29 +3,19 @@ import pgzrun
 progress = 0
 add_progress = False
 bar_background = Rect(20, 20, 300, 100)
-
-
 class ProgressBar(Rect):
     def __init__(self, left, top, width, height):
         super().__init__(left, top, width, height)
         self.progress = width
         self.add_progress = False
-
-
 bar = ProgressBar(20, 20, 0, 100)
-
-
 def draw():
     screen.draw.filled_rect(bar_background, "gray")
     
     screen.draw.filled_rect(ProgressBar(20, 20, bar.progress, 100), "red")
-
-
 def on_mouse_down(pos):
     if bar_background.collidepoint(pos):
         bar.add_progress = True
-
-
 def update():
     
     if bar.add_progress:

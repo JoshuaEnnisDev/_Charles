833065a48bba9866cddb1b2b4c0b1e992e4d6c8d
JoshuaEnnisDev <joshuaennisdev@gmail.com>
2023-11-13 20:54:56 -0600

    commit

--- clicker/clicker.py
+++ clicker/clicker.py
@@ -1,41 +1,60 @@
 import pgzrun
 
 
-
 class ProgressBar(Rect):
-    def __init__(self, left, top, width, height):
-        super().__init__(left, top, width, height)
+    def __init__(self, title, left, top, width, height):
+        super().__init__(left, top, width, height, anchor=("center", "center"))
         self.progress = width
         self.left = left
         self.top = top
         self.background = Rect(left, top, 300, height)
         self.add_progress = False
-    
+        self.title = title
+        
     def draw(self):
+        
         screen.draw.filled_rect(self.background, "gray")
-        screen.draw.filled_rect(self, "red")
+        screen.draw.filled_rect(Rect(self.left, self.top, self.progress, self.height), "red")
+        screen.draw.text(
+            self.title,
+            (self.left, self.top - 30),
+            color="green",
+            fontsize=40
+        )
+    
+    def on_click(self, pos):
+        if self.background.collidepoint(pos) and not self.add_progress:
+            self.add_progress = True
+    
+    def update_progress(self):
+        if self.add_progress:
+            self.progress += 1
+            if self.progress >= 300:
+                self.progress = 0
+                self.add_progress = False
         
 
-bar = ProgressBar(20, 20, 0, 100)
+bar = ProgressBar("Life", 20, 50, 0, 100)
+bar2 = ProgressBar("Magicstuff", 20, 250, 0, 100)
+
+bars = []
+bars.append(bar)
+bars.append(bar2)
 
 
 def draw():
     bar.draw()
+    bar2.draw()
 
 
 def on_mouse_down(pos):
-    if bar.background.collidepoint(pos):
-        bar.add_progress = True
+    for bar in bars:
+        bar.on_click(pos)
 
 
 def update():
-    
-    if bar.add_progress:
-        print("yop")
-        bar.progress += 1
-        if bar.progress >= 300:
-            bar.progress = 0
-            bar.add_progress = False
+    bar.update_progress()
+    bar2.update_progress()
 
 
 pgzrun.go()
